4848c0f28b3218ce27f0f377024fb643e5d48513
d0ndada <pintoavila09@gmail.com>
2023-03-13 14:45:21 +0100

    change from class  to fucntion components

--- src/components/BusinessList/BusinessList.jsx
+++ src/components/BusinessList/BusinessList.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import './BusinessList.css';
-import Business from '../Business/Business';
+import { Business } from '../Business/Business';
 
-class BusinessList extends React.Component {
-    render() {
-        return (
-            <div className="BusinessList">
-                <Business/>
-                <Business/>
-                <Business/>
-                <Business/>
-                <Business/>
-                <Business/>
-            </div>
-        )
-    }
+export const BusinessList = () => {
+    return (
+        <div className="BusinessList">
+            <Business />
+            <Business />
+            <Business />
+            <Business />
+            <Business />
+            <Business />
+        </div>
+    )
+
 }
+export default BusinessList;
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -2,32 +2,35 @@ import React from "react"
 
 
 const sortByOptions = {
-    'Best Match':'best_match',
-    'Highest Rated': 'review' ,
+    'Best Match': 'best_match',
+    'Highest Rated': 'review',
     'Most Reviewed': 'review_count'
 }
 
-class SearchBar extends React.Component{
-    renderSortByOptions() {
+export const SearchBar = () => {
+    const renderSortByOptions = () => {
         return Object.keys(sortByOptions).map(sortByOption => {
             let sortByOptionsValue = sortByOptions[sortByOption];
             return <li key={sortByOptionsValue} >{sortByOption}</li>
-        });
-    }
-    render(){
+        })
+    };
+
+    return (
         <div class="SearchBar">
-  <div className="SearchBar-sort-options">
-    <ul>
-      {this.renderSortByOptions}
-    </ul>
-  </div>
-  <div className="SearchBar-fields">
-    <input placeholder="Search Businesses" />
-    <input placeholder="Where?" />
-  </div>
-  <div className="SearchBar-submit">
-    <a>Let's Go</a>
-  </div>
-</div>
-    }
+            <div className="SearchBar-sort-options">
+                <ul>
+                    {renderSortByOptions}
+                </ul>
+            </div>
+            <div className="SearchBar-fields">
+                <input placeholder="Search Businesses" />
+                <input placeholder="Where?" />
+            </div>
+            <div className="SearchBar-submit">
+                <a>Let's Go</a>
+            </div>
+        </div>
+    )
 }
+
+export default SearchBar;
