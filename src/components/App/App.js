7687828dc460e3d67c336d19a13da376a92bfd48
d0ndada <pintoavila09@gmail.com>
2023-03-13 16:53:02 +0100

    modifying

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -29,7 +29,7 @@ function App() {
     <div className="App">
       <h1>ravenous</h1>
       <SearchBar />
-      <BusinessList propName={businesses} />
+      <BusinessList business={businesses} />
     </div>
   );
 }
--- src/components/Business/Business.jsx
+++ src/components/Business/Business.jsx
@@ -1,19 +1,8 @@
 import React from 'react'
 import './Business.css'
 
-const business = {
-    imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
-    name: 'MarginOtto Pizzeria',
-    address: '1010 Paddington Way',
-    city: 'Flavortown',
-    state: 'NY',
-    zipCode: '10101',
-    category: 'Italian',
-    rating: 4.5,
-    reviewCount: 90
-}
 
-export const Business = () => {
+export const Business = (props) => {
     return (<div className="Business">
         <div className="image-container">
             <img src='https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg' alt='' />
@@ -21,14 +10,14 @@ export const Business = () => {
         <h2>{business.name}</h2>
         <div className="Business-information">
             <div className="Business-address">
-                <p>{business.address}</p>
-                <p>{business.city},{business.state}</p>
-                <p>{business.zipCode}</p>
+                <p>{props.business.address}</p>
+                <p>{props.business.city},{props.business.state}</p>
+                <p>{props.business.zipCode}</p>
             </div>
             <div className="Business-reviews">
-                <h3>{business.category}</h3>
-                <h3 className="rating">{business.rating}</h3>
-                <p>{business.reviewCount}</p>
+                <h3>{props.business.category}</h3>
+                <h3 className="rating">{props.business.rating}</h3>
+                <p>{props.business.reviewCount}</p>
             </div>
         </div>
     </div>)
