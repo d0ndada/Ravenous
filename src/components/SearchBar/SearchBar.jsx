6e3b23c8b60aeae787e3bee9b0938cae7ce3a284
d0ndada <pintoavila09@gmail.com>
2023-03-13 14:22:03 +0100

    creating html & link to renderSortByOptions

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -15,6 +15,19 @@ class SearchBar extends React.Component{
         });
     }
     render(){
-        
+        <div class="SearchBar">
+  <div className="SearchBar-sort-options">
+    <ul>
+      {this.renderSortByOptions}
+    </ul>
+  </div>
+  <div className="SearchBar-fields">
+    <input placeholder="Search Businesses" />
+    <input placeholder="Where?" />
+  </div>
+  <div className="SearchBar-submit">
+    <a>Let's Go</a>
+  </div>
+</div>
     }
 }
